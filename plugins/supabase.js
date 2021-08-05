d16e39679639a54ad5bda7dbe8683c4497a5b816
Jesús García <ctw@ctwhome.com>
2021-08-05 14:29:15 +0200

    minor: alternative url, delete after

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // const SUPABASE_URL = config.publicRuntimeConfig.supabaseUrl
 // const SUPABASE_KEY = config.publicRuntimeConfig.supabaseKey
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL
-const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL || 'https://uduilifzyiujjrzlwyby.supabase.co'
+const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzNzkyMywiZXhwIjoxOTMwODEzOTIzfQ.morp5e9629xzWqRwcy1d6J3FbGbQKSwOyOAoK1URv1I'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
