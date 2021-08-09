856f84411b769b596a5fe0c62b2329816bf763f5
Jesús García <ctw@ctwhome.com>
2021-08-09 17:54:59 +0200

    fix: blog scrollbars

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen  bg-base-200">
+  <div class="flex flex-col h-screen bg-base-200">
     <header-main />
     <Nuxt class="flex-1" />
     <footer-main />
